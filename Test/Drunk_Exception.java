3db0cdabe9688a4073921174c9edb22225708d8a
June361 <49588893+June361@users.noreply.github.com>
2019-06-21 07:13:19 +0800

    1906210713
    1906210713

---
+++ Test/Drunk_Exception.java
@@ -0,0 +1,14 @@
+
+public class Drunk_Exception extends Exception
+{
+
+    public Drunk_Exception()
+    {
+        
+    }
+
+    public Drunk_Exception(String message)
+    {
+        super(message);
+    }
+}
