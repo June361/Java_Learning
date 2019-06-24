8cc2e915c1e2ec625636a4d098cc7740089be353
June361 <49588893+June361@users.noreply.github.com>
2019-06-24 23:56:26 +0800

    201906242356
    201906242356

--- Test/JForm_Collection.java
+++ Test/JForm_Collection.java
@@ -30,11 +30,62 @@ public class JForm_Collection extends JFrame {
         initComponents();
     }
 
+    List_Collection_Test list_Collection_Test=new List_Collection_Test();
+
 
     private void button1MouseClicked(MouseEvent e) {
         // TODO add your code here
-         List_Collection_Test list_Collection_Test=new List_Collection_Test();
-        list_Collection_Test.Add_List_Collection_Test();
+
+        Course_Collection_Test course_1=new Course_Collection_Test("1","数据结构");
+        list_Collection_Test.getCourse_to_Select().add(course_1);
+        Course_Collection_Test Temp_1=(Course_Collection_Test)list_Collection_Test.getCourse_to_Select().get(0);
+        System.out.println("添加了课程："+Temp_1.getID()+":"+Temp_1.getName());
+
+        Course_Collection_Test course_2=new Course_Collection_Test("2","C语言");
+        list_Collection_Test.getCourse_to_Select().add(course_2);
+        Course_Collection_Test Temp_2=(Course_Collection_Test)list_Collection_Test.getCourse_to_Select().get(1);
+        System.out.println("添加了课程："+Temp_2.getID()+":"+Temp_2.getName());
+
+        Course_Collection_Test course_3=new Course_Collection_Test("3","高等数学");
+        list_Collection_Test.getCourse_to_Select().add(course_3);
+        Course_Collection_Test Temp_3=(Course_Collection_Test)list_Collection_Test.getCourse_to_Select().get(2);
+        System.out.println("添加了课程："+Temp_3.getID()+":"+Temp_3.getName());
+
+        Course_Collection_Test course_4=new Course_Collection_Test("4","大学英语");
+        list_Collection_Test.getCourse_to_Select().add(course_4);
+        Course_Collection_Test Temp_4=(Course_Collection_Test)list_Collection_Test.getCourse_to_Select().get(3);
+        System.out.println("添加了课程："+Temp_4.getID()+":"+Temp_4.getName());
+
+        Course_Collection_Test course_5=new Course_Collection_Test("5","大学物理");
+        list_Collection_Test.getCourse_to_Select().add(course_5);
+        Course_Collection_Test Temp_5=(Course_Collection_Test)list_Collection_Test.getCourse_to_Select().get(4);
+        System.out.println("添加了课程："+Temp_5.getID()+":"+Temp_5.getName());
+
+        Course_Collection_Test course_6=new Course_Collection_Test("6","汇编语言");
+        list_Collection_Test.getCourse_to_Select().add(course_6);
+        Course_Collection_Test Temp_6=(Course_Collection_Test)list_Collection_Test.getCourse_to_Select().get(5);
+        System.out.println("添加了课程："+Temp_6.getID()+":"+Temp_6.getName());
+    }
+
+    private void button2MouseClicked(MouseEvent e) {
+        // TODO add your code here
+        int Size =list_Collection_Test.getCourse_to_Select().size();
+        System.out.println("有如下课程待选：");
+        for(int i=0;i<Size;i++){
+            Course_Collection_Test  course_Collection_Test=(Course_Collection_Test)list_Collection_Test.getCourse_to_Select().get(i);
+            System.out.println("课程："+course_Collection_Test.getID()+":"+course_Collection_Test.getName() );
+        }
+    }
+
+    private void button3MouseClicked(MouseEvent e) {
+        // TODO add your code here
+        Course_Collection_Test course_Collection_Test=new Course_Collection_Test("7","毛概");
+        list_Collection_Test.getCourse_to_Select().set(4,course_Collection_Test);
+    }
+
+    private void button4MouseClicked(MouseEvent e) {
+        // TODO add your code here
+        list_Collection_Test.getCourse_to_Select().remove(4);
     }
 
     private void initComponents() {
@@ -43,6 +94,9 @@ public class JForm_Collection extends JFrame {
         button1 = new JButton();
         scrollPane1 = new JScrollPane();
         textPane1 = new JTextPane();
+        button2 = new JButton();
+        button3 = new JButton();
+        button4 = new JButton();
 
         //======== panel1 ========
         {
@@ -64,7 +118,40 @@ public class JForm_Collection extends JFrame {
                 scrollPane1.setViewportView(textPane1);
             }
             panel1.add(scrollPane1);
-            scrollPane1.setBounds(30, 45, 625, 390);
+            scrollPane1.setBounds(30, 80, 625, 355);
+
+            //---- button2 ----
+            button2.setText("Read_Course_to_Select");
+            button2.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    button2MouseClicked(e);
+                }
+            });
+            panel1.add(button2);
+            button2.setBounds(200, 10, 156, 24);
+
+            //---- button3 ----
+            button3.setText("Modify_Course_to_Select");
+            button3.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    button3MouseClicked(e);
+                }
+            });
+            panel1.add(button3);
+            button3.setBounds(370, 10, 156, 24);
+
+            //---- button4 ----
+            button4.setText("Remove_Course_to_Select");
+            button4.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    button4MouseClicked(e);
+                }
+            });
+            panel1.add(button4);
+            button4.setBounds(30, 45, 156, 24);
 
             panel1.setPreferredSize(new Dimension(690, 455));
         }
@@ -76,6 +163,9 @@ public class JForm_Collection extends JFrame {
     private JButton button1;
     private JScrollPane scrollPane1;
     private JTextPane textPane1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
     public void JForm()
     {
--- Test/List_Collection_Test.java
+++ Test/List_Collection_Test.java
@@ -5,7 +5,6 @@ public class List_Collection_Test{
 
     private List Course_to_Select;
 
-
     public List getCourse_to_Select() {
         return this.Course_to_Select;
     }
@@ -16,17 +15,5 @@ public class List_Collection_Test{
 
     public List_Collection_Test (){
         this.Course_to_Select=new ArrayList();
-    }
-
-    public void Add_List_Collection_Test (){
-        Course_Collection_Test course_1=new Course_Collection_Test("1","数据结构");
-        Course_to_Select.add(course_1);
-        Course_Collection_Test Temp_1=(Course_Collection_Test)Course_to_Select.get(0);
-        System.out.println("添加了课程："+Temp_1.getID()+":"+Temp_1.getName());
-
-        Course_Collection_Test course_2=new Course_Collection_Test("2","C语言");
-        Course_to_Select.add(course_2);
-        Course_Collection_Test Temp_2=(Course_Collection_Test)Course_to_Select.get(1);
-        System.out.println("添加了课程："+Temp_2.getID()+":"+Temp_2.getName());
     }
 }
